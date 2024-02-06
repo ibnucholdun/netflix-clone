97418bb342052a1c7310d92380c049983cb0e6e7
ibnucholdun <ibnucholdun023@gmail.com>
2024-02-06 22:10:10 +0700

    feat: add playbutton, video player, single movie endpoint

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import { useRouter } from "next/router";
 
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
@@ -9,6 +10,8 @@ type Props = {
 };
 
 const MovieCard: React.FC<Props> = ({ data }) => {
+  const router = useRouter();
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <Image
@@ -30,7 +33,7 @@ const MovieCard: React.FC<Props> = ({ data }) => {
           <div className="flex flex-row items-center gap-3">
             <div
               className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
-              onClick={() => {}}>
+              onClick={() => router.push(`/watch/${data?.id}`)}>
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
